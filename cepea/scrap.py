ec9e78c6acf9273fd29cc48486dd1588298417b3
Rodslater <91638825+Rodslater@users.noreply.github.com>
2024-03-15 15:17:42 -0300

    Update scrap.py

--- cepea/scrap.py
+++ cepea/scrap.py
@@ -1,50 +1,33 @@
 import requests
-import time
-import csv
-import datetime
 import os
-from tqdm import tqdm
-import pyexcel_xls
-import OleFileIO_PL
-import datetime as dt
 import pandas as pd
 
- 
+from tqdm import tqdm
+from pyexcel_xls import get_data
+from datetime import datetime
+
 def download_file(url, file_name):
-    headers = {"User-agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36"}
+    headers = {"User-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36"}
     response = requests.get(url, stream=True, headers=headers)
     with open(file_name, "wb") as handle:
         for data in tqdm(response.iter_content()):
             handle.write(data)
-    handle.close()
 
+def get_cepea_data(commodity, url):
+    file_path = os.path.join('cepea', f"{commodity}.xls")
+    download_file(url, file_path)
+    data = get_data(file_path)
+    df = pd.DataFrame(data[0][3:], columns=data[0][2])
+    df["Data"] = pd.to_datetime(df["Data"], format='%d/%m/%Y')
+    df.set_index("Data", inplace=True)
+    df.to_csv(f'cepea/{commodity}.csv')
 
-dados = [
-    {'base_name': 'etanol_hidratado', 'url': 'https://www.cepea.esalq.usp.br/br/indicador/series/' + 'etanol.aspx?id=103'},
-    {'base_name': 'milho', 'url': 'https://www.cepea.esalq.usp.br/br/indicador/series/' + 'milho.aspx?id=77'}
+commodities = [
+    {'name': 'etanol_hidratado', 'url': 'https://www.cepea.esalq.usp.br/br/indicador/series/etanol.aspx?id=103'},
+    {'name': 'boi_gordo', 'url': 'https://www.cepea.esalq.usp.br/br/indicador/series/boi-gordo.aspx?id=2'},
+    {'name': 'milho', 'url': 'https://www.cepea.esalq.usp.br/br/indicador/series/milho.aspx?id=77'}
 ]
 
+for commodity in commodities:
+    get_cepea_data(commodity['name'], commodity['url'])
 
-# faz o download do excel no site do CEPEA
-for dado in dados:
-    name_file = dado['base_name'] + '_' + time.strftime("%d.%m.%Y") + '.xls'
-    path_file = os.path.join('cepea', name_file)
-    download_file(dado['url'], path_file)
-    print(path_file)
-    
-
-#função que retorna um dataframe dos dados do CEPEA
-def dados_cepea(commoditie):
-    name_file = "cepea/" + commoditie + '_' + time.strftime("%d.%m.%Y") + '.xls'
-    with open(name_file,'rb') as file:
-        ole = OleFileIO_PL.OleFileIO(file)
-        if ole.exists('Workbook'):
-            d = ole.openstream('Workbook')
-            df = pd.read_excel(d,engine='xlrd', skiprows=3)
-            df["Data"] = pd.to_datetime(df["Data"], format='%d/%m/%Y') 
-            df = df.set_index("Data")
-            return df
-        
-#Chamando a função para cada uma das commodities
-etanol_hidratado = dados_cepea('etanol_hidratado')
-milho = dados_cepea('milho')
